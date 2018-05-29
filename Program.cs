363037688cb6f241f1636aec21c5ed1fbef5dbd3
Jamal Dawson <jamal.a.dawson@gmail.com>
2018-05-28 20:17:45 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace makingDecisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isSunny = true;
+
+            if (isSunny)
+                Console.WriteLine("Wear sunglasses!");
+            else
+                Console.WriteLine("Don't wear sunglasses.");
+
+        }
+    }
+}

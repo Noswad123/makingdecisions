f7ff129e3ba41d297ec182f1eee873ddec137dee
Jamal Dawson <jamal.a.dawson@gmail.com>
2018-05-28 20:24:44 -0400

    finished

--- Program.cs
+++ Program.cs
@@ -7,11 +7,28 @@ namespace makingDecisions
         static void Main(string[] args)
         {
             bool isSunny = true;
+            bool atBeach = true;
+
 
             if (isSunny)
+            {
                 Console.WriteLine("Wear sunglasses!");
+
+                if (atBeach)
+                    Console.WriteLine("Wear sunblock!");
+                else
+                    Console.WriteLine("Don't need sunblock!");
+
+            }
             else
-                Console.WriteLine("Don't wear sunglasses.");
+            {
+                Console.WriteLine("Don't need to wear sunglasses.");
+                if (atBeach)
+                    Console.WriteLine("Come back tomorrow");
+                else
+                    Console.WriteLine("Don't go to the beach");
+            }
+
 
         }
     }
